d4bf0965b58ffe6b5725fdf166f31fa0e162ec98
k.korostylev <k.korostylev@gmail.com>
2024-08-23 10:51:29 +0500

    Domain added

--- app/src/main/java/com/korostylev/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/korostylev/shoppinglist/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.korostylev.shoppinglist
+package com.korostylev.shoppinglist.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.korostylev.shoppinglist.R
 
 
 class MainActivity : AppCompatActivity() {
