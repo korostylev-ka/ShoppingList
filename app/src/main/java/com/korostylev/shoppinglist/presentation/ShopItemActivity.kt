114c95449568a1683d3cfd636e148f56bbdac82c
k.korostylev <k.korostylev@gmail.com>
2024-08-29 22:55:19 +0500

    Added intents for add and edit

--- app/src/main/java/com/korostylev/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/korostylev/shoppinglist/presentation/ShopItemActivity.kt
@@ -1,5 +1,7 @@
 package com.korostylev.shoppinglist.presentation
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.korostylev.shoppinglist.R
@@ -8,5 +10,27 @@ class ShopItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+    }
+
+    companion object {
+
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
+        private const val MODE_EDIT = "mode edit"
+        private const val MODE_ADD = "mode_add"
+
+        fun newIntenAddItem(context: Context): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+            return intent
+        }
+
+        fun newIntenAddItem(context: Context, shopItemId: Int): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+            intent.putExtra(EXTRA_SHOP_ITEM_ID, shopItemId)
+            return intent
+        }
     }
 }
