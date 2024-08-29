32d4352813dd8671a2e8ef892b55dcba71ca81b8
k.korostylev <k.korostylev@gmail.com>
2024-08-29 15:40:19 +0500

    Added ShopItemActivity

---
+++ app/src/main/java/com/korostylev/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.korostylev.shoppinglist.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.korostylev.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
